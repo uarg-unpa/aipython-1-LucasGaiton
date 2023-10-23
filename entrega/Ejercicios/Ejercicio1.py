3bd5a70a6cf4482a1bd78d975d1b18c3f2b91d5d
Lucas Gaitón <126361172+LucasGaiton@users.noreply.github.com>
2023-10-23 19:57:14 +0000

    modificaciones

---
+++ entrega/Ejercicios/Ejercicio1.py
@@ -0,0 +1,14 @@
+#A)
+print("Las máquinas me sorprenden con mucha frecuencia")
+
+#B)
+
+#C)
+print("Tus propios mensajes")
+
+#D)
+print("23", 23)
+#Lo que sucedio fue que el primer dato mostrados son de distintos tipos uno es un string y el otro es un entero
+
+#E)
+print("Una computadora puede ser llamada 'inteligente' si logra engañar a una persona haciéndole creer que es un humano.")
--- entrega/operaciones.py
+++ entrega/operaciones.py
@@ -1,6 +1,23 @@
-print(22+33)
-print(22-33)
-print(22*33)
-print(22//33)
-print(22/33)
-print(22%33)
+# print(22+33)
+# print(22-33)
+# print(22*33)
+# print(33 // 22)
+# print(33 / 22)
+# print(22 % 33)
+# print(22 ** 33)
+
+
+#Orden de procedimiento
+# () -parentesis
+# ** -potencia 
+# *,/ -Multiplicacion y divsion 
+# +,- -suma y resta  
+
+# print(4+2*8)
+# print((4+2)*8)
+
+edad= 19
+print(id(edad))
+edad= 19+1
+print(id(edad))
+

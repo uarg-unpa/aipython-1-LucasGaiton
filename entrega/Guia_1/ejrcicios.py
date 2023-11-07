e98fb2eef441097698a9e543d5f44e4f6575c4d9
Lucas Gaitón <126361172+LucasGaiton@users.noreply.github.com>
2023-11-07 23:15:23 +0000

    Punto 1 de guia 1 hecho

--- entrega/Guia_1/ejrcicios.py
+++ entrega/Guia_1/ejrcicios.py
@@ -1,8 +1,41 @@
-print("Las máquinas me sorprenden con mucha frecuencia.")
-print(" ")
-print("tus propios mensajes")
-print("23", 23)
+#PUNTO 1
+#A)
+print("Las máquinas me sorprenden con mucha frecuencia")
 
-print("Las máquinas me sorprenden con mucha frecuencia.")
-print("Las máquinas me sorprenden con mucha frecuencia.")
-print("Las máquinas me sorprenden con mucha frecuencia.")
+#B)
+
+#C)
+print("Tus propios mensajes")
+
+#D)
+print("23", 23)
+#Lo que sucedio fue que el primer dato mostrados son de distintos tipos uno es un string y el otro es un entero
+
+#E)
+print("Una computadora puede ser llamada 'inteligente' si logra engañar a una persona haciéndole creer que es un humano.")
+#F
+print("Lucas", "Gaiton")
+#g
+print("Lucas", "Gaiton", sep=" ")
+#h
+print("calle", "numero", "codigo postal", sep="\t")
+#i
+print("calle \n numero \n codigo postal ")
+#j
+lineaSalto = "\n"
+tabulacion = "\t"
+print("feliz"+lineaSalto+tabulacion+"Navidad"+lineaSalto+tabulacion+tabulacion+"2023")
+#k
+print("Solo podemos ver poco del futuro,", "pero lo suficiente para darnos cuenta de que hay mucho que hacer")
+#I
+print("    *    ")
+print("   * *   ")
+print("  *   *  ")
+print(" *     * ")
+print("***   ***")
+print("  *   *  ")
+print("  *   *  ")
+print("  *****  ")
+
+# PUNTO 2
+
